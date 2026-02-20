fe968b1eef7b2075e6b3ffae75d09e8300cce09a
rayanaaajs <rayanajakasurya54@gmail.com>
2026-02-20 10:35:50 +0700

    Percobaan 1

---
+++ Jobsheet2/Mahasiswa25.java
@@ -0,0 +1,42 @@
+package Jobsheet2;
+
+public class Mahasiswa25 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+
+    void tampilkanInformasi() {
+        System.out.println("Nama " + nama);
+        System.out.println("NIM " + nim);
+        System.out.println("Kelas " + kelas);
+        System.out.println("IPK " + ipk);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+
+    void updateIpk(double ipkBaru){
+        if (ipk >= 0 && ipk <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
+    }
+
+    
+    String niliaKinerja(){
+        if (ipk >= 3.5) {
+            return "kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "kinerja cukup";
+        } else{
+          return "kinerja kurang";  
+        }
+    }
+}
