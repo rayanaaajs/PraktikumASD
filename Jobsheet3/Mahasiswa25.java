fa793723885503653612bb9104f280775afcbcb0
rayanaaajs <rayanajakasurya54@gmail.com>
2026-03-01 12:59:52 +0700

    Praktikum 2

--- Jobsheet3/Mahasiswa25.java
+++ Jobsheet3/Mahasiswa25.java
@@ -6,4 +6,15 @@ public class Mahasiswa25 {
     public String kelas;
     public float ipk;
 
+
+    void cetakInfo(){
+        System.out.println("NIM     : " + nim);
+        System.out.println("Nama    : " + nama);
+        System.out.println("Kelas   : " + kelas);
+        System.out.println("IPK     : " + ipk);
+        System.out.println("----------------------------------");
+    }
+
 }
+
+
