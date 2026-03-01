47e11a2ec9697cc37bf63058d77358f75186589e
rayanaaajs <rayanajakasurya54@gmail.com>
2026-03-01 14:49:34 +0700

    Praktikum 3

---
+++ Jobsheet3/MataKuliah25.java
@@ -0,0 +1,15 @@
+package Jobsheet3;
+
+public class MataKuliah25 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public MataKuliah25(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }   
+}
