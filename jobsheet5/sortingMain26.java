d321b457cf446ff984397f020ab047563fde9882
rayanaaajs <rayanajakasurya54@gmail.com>
2026-04-20 07:55:29 +0700

    Praktikum 1

---
+++ jobsheet5/sortingMain26.java
@@ -0,0 +1,32 @@
+package jobsheet5;
+
+public class sortingMain26 {
+    public static void main(String[] args) {
+        int a[] = {20, 10, 2, 7, 12};
+        int b[] = {30, 20, 2, 8, 14};
+        int c[] = {40, 10, 4, 9, 3};
+        
+        sorting26 dataUrut1 = new sorting26(a, a.length);
+        sorting26 dataUrut2 = new sorting26(b, b.length);
+        sorting26 dataUrut3 = new sorting26(c, c.length);
+
+        System.out.println("Data awal 1");
+        dataUrut1.tampil();
+        dataUrut1.bubbleSort();
+        System.out.println("data sudah diurutkan dengan bubble sort (ASC)");
+        dataUrut1.tampil();
+
+        System.out.println("Data awal 2");
+        dataUrut2.tampil();
+        dataUrut2.selectionSort();
+        System.out.println("data sudah diurutkan dengan selection sort (ASC)");
+        dataUrut2.tampil();
+
+        System.out.println("Data awal 3");
+        dataUrut3.tampil();
+        dataUrut3.selectionSort();
+        System.out.println("data sudah diurutkan dengan insertion sort (ASC)");
+        dataUrut3.tampil();
+    }
+    
+}
