649a5afec288c19cbbe878a9780fd038ba277a19
rayanaaajs <rayanajakasurya54@gmail.com>
2026-05-24 19:33:26 +0700

    Praktikum 1

---
+++ Jobsheet9/MahasiswaDemo25.java
@@ -0,0 +1,75 @@
+package Jobsheet9;
+
+import java.util.Scanner;
+
+public class MahasiswaDemo25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int pilih = 0;
+
+        StackTugasMahasiswa25 stack = new StackTugasMahasiswa25(5);
+
+        do{
+            System.out.println("\nMenu");
+            System.out.println("1. Mengumpulkan Tugas");
+            System.out.println("2. Menilai Tugas");
+            System.out.println("3. Melihat Tugas Teratas");
+            System.out.println("4. Melihat Daftar Tugas");
+            System.out.println("Pilih: ");
+            pilih = sc.nextInt();
+            sc.nextLine();
+            switch (pilih) {
+                case 1:
+                    System.out.print("Nama: ");
+                    String nama = sc.nextLine();
+                    System.out.print("NIM: ");
+                    String nim = sc.nextLine();
+                    System.out.print("Kelas: ");
+                    String kelas = sc.nextLine();
+
+                    Mahasiswa25 mhs = new Mahasiswa25(nama, nim, kelas);
+                    stack.push(mhs);
+
+                    System.out.printf("Tugas %s berhasil dikumpulkan\n", mhs.nama);
+                    break;
+
+                case 2:
+                    Mahasiswa25 dinilai = stack.pop();
+
+                    if (dinilai != null) {
+                        System.out.println("Menilai tugas dari " + dinilai.nama);
+                        System.out.print("Masukkan nilai (0-100): ");
+
+                        int nilai = sc.nextInt();
+
+                        dinilai.tugasDinilai(nilai);
+
+                        System.out.printf("Nilai Tugas %s adalah %d\n", dinilai.nama, nilai);
+                    }
+                    break;
+
+                case 3:
+                    Mahasiswa25 lihat = stack.peek();
+
+                    if (lihat != null) {
+                        System.out.println("Tugas terakhir dikumpulkan oleh " + lihat.nama);
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Daftar semua tugas");
+                    System.out.println("Nama\tNIM\tKelas");
+
+                    stack.print();
+                    break;
+
+                default:
+                System.out.println("Pilihan tidak valid.");
+            }
+
+
+        } while (pilih >= 1 && pilih <= 4);
+
+        sc.close();
+    }
+}
---
+++ Jobsheet9/StackTugasMahasiswa25.java
@@ -0,0 +1,66 @@
+package Jobsheet9;
+
+public class StackTugasMahasiswa25 {
+    Mahasiswa25[] stack;
+    int top;
+    int size;
+
+    public StackTugasMahasiswa25(int size){
+        this.size = size;
+        stack = new Mahasiswa25[size];
+        top = -1;
+    }
+
+    public boolean isFull() {
+        if (top == size -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void push(Mahasiswa25 mhs){
+        if (!isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack Penuh!, tidak dapat menambah tugas lagi");
+        }
+    }
+
+    public Mahasiswa25 pop() {
+        if (!isEmpty()) {
+            Mahasiswa25 m = stack[top];
+            top--;
+            return m;
+        } else {
+            System.out.println("Stack Kosong!, tidak ada tugas untuk dinilai");
+            return null;
+        }
+    }
+
+
+    public Mahasiswa25 peek(){
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack Kosong!, tidak ada tugas yang dikumpulkan");
+            return null;
+        }
+    }
+
+    public void print(){
+        for (int i = 0; i <= top; i++) {
+            System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+        }
+        System.out.println("");
+    }
+}
